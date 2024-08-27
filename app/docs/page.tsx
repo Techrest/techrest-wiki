f8068ce524773f51d5a8c79ad7643dca4e2f2086
lucasdaher <contato@lucasdaher.com>
2024-08-27 19:23:22 -0300

    chore: adicionando roteamento para documentação.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/header";
 
 interface DocsProps {}
 
 export default function Docs({}: DocsProps) {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
