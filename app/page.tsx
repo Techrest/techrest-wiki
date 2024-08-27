a94a7fe38afeefa1566f67b3dca2dfbf351aa6f0
lucasdaher <contato@lucasdaher.com>
2024-08-27 19:25:22 -0300

    chore: alterando estutura principal de páginas.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <React.Fragment>
       <Header />
+
+      <main>
+        <section>
+          <h1>Introdução</h1>
+          <p>
+            Seja bem-vindo à página Wiki da Techrest. Aqui você encontrará todas
+            as informações relacionadas ao nosso projeto.
+          </p>
+        </section>
+      </main>
     </React.Fragment>
   );
 }
