dba35cb45919299f2b7528b2ca7544abd27bf0c8
lucasdaher <contato@lucasdaher.com>
2024-08-27 18:11:11 -0300

    feat(header): adicionando navegação ao header.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -12,20 +12,32 @@ export default function Header({}: HeaderProps) {
   return (
     <header className="bg-black-100 w-full min-h-14 h-auto py-2 px-9 flex justify-between items-center">
       <div className="flex justify-center items-center gap-9">
-        <Image src={techrestLogo} alt="Techrest Logo" />
+        <Image
+          src={techrestLogo}
+          alt="Techrest Logo"
+          className="h-auto leading-none"
+        />
         <nav>
-          <ul className="flex justify-center items-center gap-4">
+          <ul className="flex justify-center items-center gap-4 leading-none">
             <Link href="/docs">
-              <li className="text-white">Docs</li>
+              <li className="text-white/50 text-sm hover:text-white transition-all duration-200">
+                Docs
+              </li>
             </Link>
             <Link href="/comunidade">
-              <li className="text-white/50">Comunidade</li>
+              <li className="text-white/50 text-sm hover:text-white transition-all duration-200">
+                Comunidade
+              </li>
             </Link>
             <Link href="/tutoriais">
-              <li className="text-white/50">Tutoriais</li>
+              <li className="text-white/50 text-sm hover:text-white transition-all duration-200">
+                Tutoriais
+              </li>
             </Link>
             <Link href="/ajuda">
-              <li className="text-white/50">Ajuda</li>
+              <li className="text-white/50 text-sm hover:text-white transition-all duration-200">
+                Ajuda
+              </li>
             </Link>
           </ul>
         </nav>
