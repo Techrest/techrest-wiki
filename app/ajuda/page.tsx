13d6813d8e6a2437d6b9fb0633a377a3c774e38d
rSaydelleS <rafael.ppuerari@gmail.com>
2024-08-27 21:30:09 -0300

    feat: desenvolvendo a aberutra das perguntas.

--- app/ajuda/page.tsx
+++ app/ajuda/page.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Header from "../components/header";
 import arrow_close from "../assets/arrow-close.svg";
 import arrow_open from "../assets/arrow-open.svg";
+import buttonExclamation from "../assets/buttonExclamation.svg";
+import buttonNote from "../assets/buttonNote.svg";
 import Image from "next/image";
 
 interface AjudaProps {}
 
 export default function Ajuda({}: AjudaProps) {
+  const [openSocialHelp, setOpenSocialHelp] = useState(true);
+  const [openPasswordlHelp, setOpenPasswordlHelp] = useState(true);
+  const [openEmailHelp, setOpenEmailHelp] = useState(true);
+
   return (
     <>
       <Header />
@@ -28,21 +36,27 @@ export default function Ajuda({}: AjudaProps) {
                 Como posso conectar minha conta Techrest com outras redes
                 sociais?
               </h2>
-              <Image src={arrow_open} alt="Flecha para abrir os detalhes" />
+              <Image
+                src={openSocialHelp == true ? arrow_open : arrow_close}
+                alt="Flecha para abrir os detalhes"
+                onClick={() => setOpenSocialHelp(!openSocialHelp)}
+              />
             </div>
             <div>
-              <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
+              {openSocialHelp == false && (
+                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              )}
               <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
             </div>
           </section>
@@ -51,22 +65,28 @@ export default function Ajuda({}: AjudaProps) {
               <h2 className="text-xl">
                 Esqueci minha senha, como faço para acessar minha conta?
               </h2>
-              <Image src={arrow_open} alt="Flecha para abrir os detalhes" />
+              <Image
+                src={openPasswordlHelp == true ? arrow_open : arrow_close}
+                alt="Flecha para abrir os detalhes"
+                onClick={() => setOpenPasswordlHelp(!openPasswordlHelp)}
+              />
             </div>
             <div>
-              <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-              <hr className="bg-white/50 w-auto h-[0.1rem] mt-5" />
+              {openPasswordlHelp == false && (
+                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              )}
+              <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
             </div>
           </section>
           <section className="max-w-5xl">
@@ -74,23 +94,58 @@ export default function Ajuda({}: AjudaProps) {
               <h2 className="text-xl">
                 Como posso alterar o email da minha conta?
               </h2>
-              <Image src={arrow_open} alt="Flecha para abrir os detalhes" />
+              <Image
+                src={openEmailHelp == true ? arrow_open : arrow_close}
+                alt="Flecha para abrir os detalhes"
+                onClick={() => setOpenEmailHelp(!openEmailHelp)}
+              />
             </div>
             <div>
-              <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-              <hr className="bg-white/50 w-auto h-[0.1rem] mt-5" />
+              {openEmailHelp == false && (
+                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              )}
+              <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
             </div>
+          </section>
+        </section>
+        <section className="flex  text-white m-6 gap-10">
+          <section className="flex flex-col justify-between gap-5 max-w-80 p-6 border-2 border-black-300 rounded-md bg-black">
+            <h1 className="text-xl">Abertura de solicitação</h1>
+            <p className="text-white/50">
+              Abra uma solicitação e conte conosco para resolver qualquer
+              necessidade ou dúvida. Estamos aqui para ajudar e garantir que
+              você tenha a melhor experiência possível.
+            </p>
+            <button className="flex justify-center p-3 bg-white text-black-100 rounded-md">
+              <Image
+                src={buttonNote}
+                alt="Enviar solicitação para resolução de problemas"
+              />{" "}
+              Abrir solicitação
+            </button>
+          </section>
+          <section className="flex flex-col justify-between gap-5 max-w-80 p-6 border-2 border-black-300 rounded-md bg-black">
+            <h1 className="text-xl">Sugestões e feedbacks</h1>
+            <p className="text-white/50">
+              Sua opinião é essencial para nós! Deixe suas sugestões e feedbacks
+              para que possamos melhorar continuamente e oferecer a melhor
+              experiência possível.
+            </p>
+            <button className="flex justify-center p-3 bg-white text-black-100 rounded-md">
+              <Image src={buttonExclamation} alt="Enviar o feedback" /> Enviar
+              feedback
+            </button>
           </section>
         </section>
       </main>
