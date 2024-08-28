4cb6070861d89021628ad20933c11669f40b3a19
lucasdaher <contato@lucasdaher.com>
2024-08-28 16:14:23 -0300

    feat(helpcenter): atualizando accordion e adicionando nova estilização.

--- app/ajuda/page.tsx
+++ app/ajuda/page.tsx
@@ -2,153 +2,41 @@
 
 import React, { useState } from "react";
 import Header from "../components/header";
-import arrow_close from "../assets/arrow-close.svg";
-import arrow_open from "../assets/arrow-open.svg";
-import buttonExclamation from "../assets/buttonExclamation.svg";
-import buttonNote from "../assets/buttonNote.svg";
+
 import Image from "next/image";
+import Faq from "@/components/help_center/faq";
+import Cards from "@/components/help_center/cards";
 
-interface AjudaProps {}
-
-export default function Ajuda({}: AjudaProps) {
+/**
+ * Renders the help page.
+ *
+ * @returns The rendered Ajuda component.
+ */
+export default function Ajuda() {
   const [openSocialHelp, setOpenSocialHelp] = useState(true);
   const [openPasswordlHelp, setOpenPasswordlHelp] = useState(true);
   const [openEmailHelp, setOpenEmailHelp] = useState(true);
 
   return (
-    <>
+    <React.Fragment>
       <Header />
-      <main className="flex flex-col m-16 text-white gap-10 ">
-        <section className="flex flex-col gap-5 sm:m-1">
-          <h1 className=" text-4xl">Ajuda</h1>
-          <p className="text-white/50 text-start text-lg">
-            Aqui você encontrará respostas rápidas e soluções eficazes para as
-            suas dúvidas e problemas.
-            <br /> Nós oferecemos a assistência necessária para garantir que
-            você tenha a melhor experiência possível com a nossa plataforma
-          </p>
+
+      {/* Seção principal da página */}
+      <main className="max-w-[1440px] mx-auto flex flex-col m-16 text-white gap-10 ">
+        {/* Seção de introdução do tema abordado na página */}
+        <section>
+          <div>
+            <h1 className="font-manrope font-bold text-4xl leading-normal">
+              Central de Ajuda
+            </h1>
+          </div>
         </section>
-        <section className="flex flex-col gap-10">
-          <h1 className="text-xl">Dúvidas frequentes</h1>
-          <section className="max-w-5xl">
-            <div className="flex justify-between">
-              <h2 className="text-xl">
-                Como posso conectar minha conta Techrest com outras redes
-                sociais?
-              </h2>
-              <Image
-                src={openSocialHelp == true ? arrow_open : arrow_close}
-                alt="Flecha para abrir os detalhes"
-                onClick={() => setOpenSocialHelp(!openSocialHelp)}
-              />
-            </div>
-            <div>
-              {openSocialHelp == false && (
-                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-              )}
-              <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
-            </div>
-          </section>
-          <section className="max-w-5xl">
-            <div className="flex justify-between ">
-              <h2 className="text-xl">
-                Esqueci minha senha, como faço para acessar minha conta?
-              </h2>
-              <Image
-                src={openPasswordlHelp == true ? arrow_open : arrow_close}
-                alt="Flecha para abrir os detalhes"
-                onClick={() => setOpenPasswordlHelp(!openPasswordlHelp)}
-              />
-            </div>
-            <div>
-              {openPasswordlHelp == false && (
-                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-              )}
-              <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
-            </div>
-          </section>
-          <section className="max-w-5xl">
-            <div className="flex justify-between">
-              <h2 className="text-xl">
-                Como posso alterar o email da minha conta?
-              </h2>
-              <Image
-                src={openEmailHelp == true ? arrow_open : arrow_close}
-                alt="Flecha para abrir os detalhes"
-                onClick={() => setOpenEmailHelp(!openEmailHelp)}
-              />
-            </div>
-            <div>
-              {openEmailHelp == false && (
-                <p className="text-white/50 text-start text-sm w-3/4 max-w-4xl min-w-80 pt-6">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-              )}
-              <hr className="bg-white/50 w- h-[0.1rem] mt-5" />
-            </div>
-          </section>
-        </section>
-        <section className="flex flex-col text-white m-e gap-5 md:flex-row md:gap-10 md:m-6 ">
-          <section className="flex flex-col justify-between gap-5 max-w-80 p-6 border-2 border-black-300 rounded-md bg-black">
-            <h1 className="text-xl">Abertura de solicitação</h1>
-            <p className="text-white/50">
-              Abra uma solicitação e conte conosco para resolver qualquer
-              necessidade ou dúvida. Estamos aqui para ajudar e garantir que
-              você tenha a melhor experiência possível.
-            </p>
-            <button className="flex justify-center p-3 bg-white text-black-100 rounded-md">
-              <Image
-                src={buttonNote}
-                alt="Enviar solicitação para resolução de problemas"
-              />{" "}
-              Abrir solicitação
-            </button>
-          </section>
-          <section className="flex flex-col justify-between gap-5 max-w-80 p-6 border-2 border-black-300 rounded-md bg-black">
-            <h1 className="text-xl">Sugestões e feedbacks</h1>
-            <p className="text-white/50">
-              Sua opinião é essencial para nós! Deixe suas sugestões e feedbacks
-              para que possamos melhorar continuamente e oferecer a melhor
-              experiência possível.
-            </p>
-            <button className="flex justify-center p-3 bg-white text-black-100 rounded-md">
-              <Image src={buttonExclamation} alt="Enviar o feedback" /> Enviar
-              feedback
-            </button>
-          </section>
-        </section>
+
+        {/* Seção de dúvidas frequentes */}
+        <Faq />
+
+        <Cards />
       </main>
-    </>
+    </React.Fragment>
   );
 }
---
+++ components/help_center/cards.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+
+import penIcon from "../../app/assets/pen.svg";
+import userHeart from "../../app/assets/userHeart.svg";
+import React from "react";
+
+export default function Cards() {
+  const data = [
+    {
+      id: 1,
+      title: "Abertura de solicitação",
+      desc: "Abra uma solicitação e conte conosco para resolver qualquer necessidade ou dúvida. Estamos aqui para ajudar e garantir que você tenha a melhor experiência possível.",
+      icon: {
+        name: penIcon,
+        alt: "Pen Icon",
+      },
+      button: {
+        text: "Abrir solicitação",
+        className: "",
+      },
+    },
+    {
+      id: 2,
+      title: "Sugestões e feedbacks",
+      desc: "Sua opinião é essencial para nós! Deixe suas sugestões e feedbacks para que possamos melhorar continuamente e oferecer a melhor experiência possível.",
+      icon: {
+        name: userHeart,
+        alt: "User Heart Icon",
+      },
+      button: {
+        text: "Enviar feedback",
+        className:
+          "bg-sunset-500 hover:bg-sunset-300 transition-all duration-200",
+      },
+    },
+  ];
+  return (
+    <section className="flex flex-col gap-4 md:flex-row md:gap-4 text-white w-full px-4 md:mt-16 mt-8">
+      {data.map((card) => (
+        <React.Fragment key={card.id}>
+          {/* Cartão */}
+          <div className="flex flex-col justify-center items-start gap-4 max-w-[456px] w-full h-full p-6 border border-black-300 rounded-lg bg-black-50">
+            {/* Titulo do cartão */}
+            <div className="flex flex-col justify-center items-start gap-2 w-full">
+              <h1 className="font-manrope font-bold text-xl text-white leading-none">
+                {card.title}
+              </h1>
+              <p className="font-inter font-normal text-base text-white/60 tracking-[-0.04rem] max-w-[19.625rem] w-full leading-normal">
+                {card.desc}
+              </p>
+            </div>
+            {/* Botão de ação do cartão */}
+            <button
+              className={`flex justify-center items-center gap-2 w-full px-4 py-3 bg-white text-black-100 rounded-lg hover:bg-white/80 transition-all duration-200 ${card.button.className}`}
+            >
+              <Image
+                src={card.icon.name}
+                alt={card.icon.alt}
+                className="w-5 h-5"
+              />
+              <span className="font-inter font-semibold text-base text-black-100">
+                {card.button.text}
+              </span>
+            </button>
+          </div>
+        </React.Fragment>
+      ))}
+    </section>
+  );
+}
