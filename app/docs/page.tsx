d81f422bb9d1e530d01c937c7e3d5c0e5cd6054e
lucasdaher <contato@lucasdaher.com>
2024-08-28 20:10:55 -0300

    test: testando novo componente de botão na página docs.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Header from "../../components/header";
+import Button from "@/components/techrest/button";
+
+import giftIcon from "../../assets/icons/gift.svg";
 
 interface DocsProps {}
 
@@ -7,6 +10,14 @@ export default function Docs({}: DocsProps) {
   return (
     <>
       <Header />
+
+      <Button
+        variant="primary"
+        text="Editar publicação"
+        icon={true}
+        iconSrc={giftIcon}
+        iconPosition="left"
+      />
     </>
   );
 }
