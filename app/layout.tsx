42a33410bb71cc3b07b4e518b282c72220a70942
lucasdaher <contato@lucasdaher.com>
2024-08-30 15:27:51 -0300

    feat: atualizando o ícone da página.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={`${inter.className} ${manrope.className} bg-black-50`}>
         {children}
       </body>
