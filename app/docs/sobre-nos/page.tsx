df0e287fd1b26062577869cf5aa3bc81ee0870df
lucasdaher <contato@lucasdaher.com>
2024-08-30 18:21:49 -0300

    feat: implementando modelo de tópicos da documentação.

--- app/docs/sobre-nos/page.tsx
+++ app/docs/sobre-nos/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/header";
+import Header from "../../../components/header";
 
 import Sidebar from "@/components/techrest/sidebar/sidebar";
 import {
@@ -10,9 +10,10 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import Link from "next/link";
 import Button from "@/components/techrest/button";
 import { ChevronRightIcon } from "@heroicons/react/16/solid";
-import Link from "next/link";
+import { ChevronLeftIcon } from "@heroicons/react/16/solid";
 
 interface DocsProps {}
 
@@ -21,8 +22,9 @@ export default function Docs({}: DocsProps) {
     <>
       <Header />
 
-      <Sidebar />
+      <Sidebar topic="sobre-nos" />
 
+      {/* Conteúdo principal que será renderizado */}
       {/* Conteúdo principal que será renderizado */}
       <main className="pl-60 w-full">
         <div className="text-white fill-white p-8 max-w-[1440px]">
@@ -39,14 +41,14 @@ export default function Docs({}: DocsProps) {
               <BreadcrumbSeparator className="text-white/50" />
               <BreadcrumbItem>
                 <BreadcrumbPage className="text-white">
-                  Introdução
+                  Sobre nós
                 </BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
           <div className="flex flex-col justify-center items-start gap-2 w-full">
             <h1 className="text-manrope text-4xl font-bold text-white mt-5">
-              Introdução
+              Sobre nós
             </h1>
             <div className="flex flex-col justify-center items-start gap-4 mt-2">
               <p className="text-white/50 text-base font-inter font-normal w-full">
@@ -66,18 +68,32 @@ export default function Docs({}: DocsProps) {
               </p>
             </div>
           </div>
-          <Link href={`/docs/sobre-nos`}>
-            <Button
-              variant="primary"
-              hasText={true}
-              text="Avançar para o próximo tópico"
-              width="[23rem]"
-              icon={true}
-              iconPosition="right"
-              iconSrc={<ChevronRightIcon className="w-6 h-6 fill-black-50" />}
-              primaryStyle="mt-14"
-            />
-          </Link>
+
+          <div className="flex justify-start items-center gap-2">
+            <Link href={`/docs`}>
+              <Button
+                variant="primary"
+                hasText={false}
+                width="[23rem]"
+                icon={true}
+                iconPosition="left"
+                iconSrc={<ChevronLeftIcon className="w-6 h-6 fill-black-50" />}
+                primaryStyle="mt-14"
+              />
+            </Link>
+            <Link href={`/docs/equipes`}>
+              <Button
+                variant="primary"
+                hasText={true}
+                text="Avançar para o próximo tópico"
+                width="[23rem]"
+                icon={true}
+                iconPosition="right"
+                iconSrc={<ChevronRightIcon className="w-6 h-6 fill-black-50" />}
+                primaryStyle="mt-14"
+              />
+            </Link>
+          </div>
         </div>
       </main>
     </>
